040c4654f9013d33efc31de06f633f34d84e15df
Quentin BONHOURE <bonhoure.quentin@gmail.com>
2026-06-29 18:37:00 +0200

    [EDIT] Modification de la taille des images dans le détail d'un article

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -4,7 +4,7 @@
 <div class="max-w-3xl mx-auto p-4">
 	<h1 class="text-2xl font-bold mb-2">{{ $article->title }}</h1>
 	@if($article->image)
-		<img src="{{ $article->image }}" alt="{{ $article->title }}" class="mb-3 rounded max-w-sm w-full">
+		<img src="{{ $article->image }}" alt="{{ $article->title }}" class="mb-3 rounded w-48 object-cover">
 	@endif
 	<p class="mb-2">{{ $article->description }}</p>
 	<p class="mb-1">Catégorie : {{ $article->category->name }}</p>
